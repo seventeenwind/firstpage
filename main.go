e92ac643769472cc4f34e933c36add304c4a2e6b
刘先生 <857092803@qq.com>
2019-04-16 01:28:25 +0800

    http main.go

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
+// http 包建立web 服务器
 package main
 
-import "fmt"
+import (
+	"io"
+	"net/http"
+)
 
-func main() {
-	fmt.Printf("hello,world\n")
+func firstPage(w http.ResponseWriter, r *http.Request){
+	io.WriteString(w,"<h1>hello,this is my first pagehahaha!</h1>")
+}
+
+func main()  {
+	http.HandleFunc("/",firstPage)
+	http.ListenAndServe(":8000",nil)
 }
